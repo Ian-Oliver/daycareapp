eeed3cc1822092029bcb662ef1fd30d602007b76
Ian Oliver <ian.oliver@flawlessfuture.com>
2017-09-22 20:56:04 -0400

    dadwa

---
+++ DayCareApp/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DayCareApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-HRLD5C6\SQLEXPRESS;Initial Catalog=DayCare;Integrated Security=True");
+            conn.Open();
+            SqlCommand command = new SqlCommand("Select * from Employees", conn);
+            var reader = command.ExecuteReader();
+
+            conn.Close();
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
